58606a064298dfeffe24e7a83996a34333a594de
Jacinth Mahanta <96361743+Jacinth-Mahanta@users.noreply.github.com>
2022-07-07 07:30:23 +0530

    Add files via upload

---
+++ src/components/Notes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Lorem</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt illo fugiat magnam accusamus quisquam eius! Neque eos dolorem illo illum quo quisquam ea animi rerum voluptatum. Quibusdam aliquid sapiente consequatur.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
